87401417ca62bda5ca2f3c76bd0614dfbe91351a
p-rosit <pontus.rosqvist@gmail.com>
2024-09-04 18:04:30 +0200

    move utils

--- src/array.zig
+++ src/array.zig
@@ -1,7 +1,7 @@
 const array = @import("array/array.zig");
+pub const utils = array.utils;
 pub const Config = array.Config;
 pub const ConfigInternal = array.ConfigInternal;
-pub const utils = array.utils;
 pub const Error = array.Error;
 
 pub fn Array(comptime dtype: type, comptime config: Config(dtype)) type {
