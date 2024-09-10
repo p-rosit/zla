c9182f7ebcb90b152f7508b197592da775898eb1
p-rosit <pontus.rosqvist@gmail.com>
2024-09-10 08:20:14 +0200

    optimized elementwise operators for aligned arrays

---
+++ src/blas/aligned.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+pub fn add(comptime Array: type, self: Array, other: Array) !Array {
+    assert(self.elementWiseCompatible(other));
+    return operator(Array.config.dtype, Array, self, other, Array.config.arithmetic.add);
+}
+
+pub fn sub(comptime Array: type, self: Array, other: Array) !Array {
+    assert(self.elementWiseCompatible(other));
+    return operator(Array.config.dtype, Array, self, other, Array.config.arithmetic.sub);
+}
+
+pub fn mul(comptime Array: type, self: Array, other: Array) !Array {
+    assert(self.elementWiseCompatible(other));
+    return operator(Array.config.dtype, Array, self, other, Array.config.arithmetic.mul);
+}
+
+pub fn div(comptime Array: type, self: Array, other: Array) !Array {
+    assert(self.elementWiseCompatible(other));
+    return operator(Array.config.dtype, Array, self, other, Array.config.arithmetic.div);
+}
+
+inline fn operator(
+    dtype: type,
+    comptime Array: type,
+    self: Array,
+    other: Array,
+    comptime op: fn (dtype, dtype) callconv(.Inline) dtype,
+) !Array {
+    const result = try Array.init(self.allocator, self.shape);
+
+    for (0.., self.data, other.data) |i, v1, v2| {
+        result.data[i] = op(v1, v2);
+    }
+
+    return result;
+}
