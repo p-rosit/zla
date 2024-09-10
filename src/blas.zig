c6a92ab702e375f9b53eaea8096d73bd6decd75d
p-rosit <pontus.rosqvist@gmail.com>
2024-09-10 08:20:50 +0200

    choose between manual and aligned implementation

--- src/blas.zig
+++ src/blas.zig
@@ -1,40 +1,49 @@
 const array = @import("array/array.zig");
+const aligned = @import("blas/aligned.zig");
 const manual = @import("blas/manual.zig");
 const openblas = @import("blas/openblas.zig");
 
 pub fn add(comptime Array: type, self: Array, other: Array) !Array {
-    return switch (Array.config.blas) {
-        .manual => manual.add(Array, self, other),
-        .openblas => openblas.add(Array, self, other),
-    };
+    if (self.elementWiseCompatible(other)) {
+        return aligned.add(Array, self, other);
+    } else {
+        return manual.add(Array, self, other);
+    }
 }
 
 pub fn sub(comptime Array: type, self: Array, other: Array) !Array {
-    return switch (Array.config.blas) {
-        .manual => manual.sub(Array, self, other),
-        .openblas => openblas.sub(Array, self, other),
-    };
+    if (self.elementWiseCompatible(other)) {
+        return aligned.sub(Array, self, other);
+    } else {
+        return manual.sub(Array, self, other);
+    }
 }
 
 pub fn mul(comptime Array: type, self: Array, other: Array) !Array {
-    return switch (Array.config.blas) {
-        .manual => manual.mul(Array, self, other),
-        .openblas => openblas.mul(Array, self, other),
-    };
+    if (self.elementWiseCompatible(other)) {
+        return aligned.mul(Array, self, other);
+    } else {
+        return manual.mul(Array, self, other);
+    }
 }
 
 pub fn div(comptime Array: type, self: Array, other: Array) !Array {
-    return switch (Array.config.blas) {
-        .manual => manual.div(Array, self, other),
-        .openblas => openblas.div(Array, self, other),
-    };
+    if (self.elementWiseCompatible(other)) {
+        return aligned.div(Array, self, other);
+    } else {
+        return manual.div(Array, self, other);
+    }
 }
 
 pub fn matMul(comptime Array: type, self: Array, other: Array) !Array {
-    return switch (Array.config.blas) {
-        .manual => manual.matMul(Array, self, other),
-        .openblas => openblas.matMul(Array, self, other),
-    };
+    if (self.isBlasable() and other.isBlasable()) {
+        return switch (Array.config.blas) {
+            .manual => manual.matMul(Array, self, other),
+            .openblas => openblas.matMul(Array, self, other),
+        };
+    } else {
+        return manual.matMul(Array, self, other);
+    }
 }
 
 test "blas-internal" {
