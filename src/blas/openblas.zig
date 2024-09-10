adf3f20d768270c9815aae168688b521586894e8
p-rosit <pontus.rosqvist@gmail.com>
2024-09-10 08:21:25 +0200

    openblas operators not needed

--- src/blas/openblas.zig
+++ src/blas/openblas.zig
@@ -1,27 +1,3 @@
-pub fn add(comptime Array: type, self: Array, other: Array) !Array {
-    _ = self;
-    _ = other;
-    @compileError("Not implemented");
-}
-
-pub fn sub(comptime Array: type, self: Array, other: Array) !Array {
-    _ = self;
-    _ = other;
-    @compileError("Not implemented");
-}
-
-pub fn mul(comptime Array: type, self: Array, other: Array) !Array {
-    _ = self;
-    _ = other;
-    @compileError("Not implemented");
-}
-
-pub fn div(comptime Array: type, self: Array, other: Array) !Array {
-    _ = self;
-    _ = other;
-    @compileError("Not implemented");
-}
-
 pub fn matMul(comptime Array: type, self: Array, other: Array) !Array {
     _ = self;
     _ = other;
